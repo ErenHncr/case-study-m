e9d11dd0a0b8be211f77ace0cb9dbaac379894af
Eren Hançer <erenhncr@gmail.com>
2025-05-15 15:16:01 +0300

    add axios and mock adapter

---
+++ src/lib/axios.ts
@@ -0,0 +1,20 @@
+import axios from "axios"
+import AxiosMockAdapter from "axios-mock-adapter"
+
+import mockUsers from "../_mock_/users.json"
+import mockProducts from "../_mock_/products.json"
+
+export const axiosMockInstance = axios.create();
+
+// This sets the mock adapter on the default instance
+export const axiosMockAdapterInstance = new AxiosMockAdapter(axiosMockInstance, {
+  delayResponse: 1000,
+})
+
+axiosMockAdapterInstance.onGet("/users").reply(200, {
+  users: mockUsers,
+})
+
+axiosMockAdapterInstance.onGet("/products").reply(200, {
+  products: mockProducts,
+})
