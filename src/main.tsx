7228b822af5b6dbd0b697df5ed10f4fbe6822298
Eren Hançer <erenhncr@gmail.com>
2025-05-15 14:18:53 +0300

    add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router"
 import { Provider } from "react-redux"
 import { App } from "./App"
 import { store } from "./app/store"
@@ -13,7 +14,12 @@ if (container) {
   root.render(
     <StrictMode>
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </StrictMode>,
   )
